4f14b7e5662b91f7545e09c64dd5454dc2796ac6
huihaoweishui@163.com <huihaoweishui@163.com>
2019-08-09 14:29:47 +0800

    test jenkins

--- src/main/java/com/home/springcloud/skeletonregistry/SkeletonRegistryApplication.java
+++ src/main/java/com/home/springcloud/skeletonregistry/SkeletonRegistryApplication.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class SkeletonRegistryApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(SkeletonRegistryApplication.class, args);
     }
 
